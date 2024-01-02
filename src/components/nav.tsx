74001bbc554d0716f1ef5acedde3d4b8c244fc11
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-02 13:32:53 +0100

    refactor: refactored typescript code for buttons in nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,31 +5,24 @@ import { usePathname } from 'next/navigation';
 import { CgGym } from 'react-icons/cg';
 import { MdOutlineQueryStats } from 'react-icons/md';
 
-interface ButtonProps {
-  name: string;
-  icon: JSX.Element;
-}
-
-const Nav = () => {
-  const Button = ({ name, icon }: ButtonProps) => {
+export default function Nav() {
+  function Button(props: { href: string; icon: JSX.Element }) {
     return (
       <Link
-        href={`${name}`}
+        href={`${props.href}`}
         className={`${
-          usePathname() === name && 'bg-primary'
-        } w-1/2 h-full text-4xl grid place-items-center`}
+          usePathname() === props.href && 'bg-primary'
+        } grow text-4xl grid place-items-center`}
       >
-        {icon}
+        {props.icon}
       </Link>
     );
-  };
+  }
 
   return (
     <nav className='fixed bottom-0 w-screen h-20 border-t border-primary flex'>
-      <Button name='/' icon={<CgGym />} />
-      <Button name='/stats' icon={<MdOutlineQueryStats />} />
+      <Button href='/' icon={<CgGym />} />
+      <Button href='/stats' icon={<MdOutlineQueryStats />} />
     </nav>
   );
-};
-
-export default Nav;
+}
