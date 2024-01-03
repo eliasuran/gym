6feea3b2f3be4e860a962c6dcff0d18db5c662bb
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-03 20:05:24 +0100

    feat: completed search feature, server side rendered first, then searching client side

--- src/app/lib/components/home/searchExercise.tsx
+++ src/app/lib/components/home/searchExercise.tsx
@@ -3,22 +3,28 @@ import type { Exercises } from '@/app/lib/types/Exercise';
 import { useEffect, useState } from 'react';
 
 export default function SearchExercise(props: { exercises: Exercises[] }) {
-  const [search, setSearch] = useState('');
+  const [filteredExercises, setFilteredExercises] = useState<Exercises[]>(
+    props.exercises,
+  );
 
-  useEffect(() => {
-    props.exercises.filter((exercise) => {
-      return exercise.exercise.toLowerCase().includes(search.toLowerCase());
-    });
-  }, [search]);
+  function searchExercise(e: React.ChangeEvent<HTMLInputElement>) {
+    setFilteredExercises(
+      props.exercises.filter((exercise) => {
+        return exercise.exercise
+          .toLowerCase()
+          .includes(e.target.value.toLowerCase());
+      }),
+    );
+  }
 
   return (
     <div className='bg-primary rounded-xl overflow-hidden'>
       <input
         className='h-12 w-full p-2 bg-primary outline-none'
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={(e) => searchExercise(e)}
         placeholder='Search for exercise'
       />
-      {props.exercises.map((exercise) => (
+      {filteredExercises.slice(0, 5).map((exercise) => (
         <button
           className='h-12 w-full p-2 focus:bg-secondary outline-none'
           key={exercise.id}
